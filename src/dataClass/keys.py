36f6835ae46836315c7b58520fdc649cf7a88529
SobhanHosseini <sobhan@Sobhan>
2023-02-25 12:23:06 +0330

    fix bug

--- src/dataClass/keys.py
+++ src/dataClass/keys.py
@@ -3,6 +3,6 @@ from dataclasses import dataclass
 @dataclass
 class Keys:
     random_connect:str = ':bust_in_silhouette: Random Connect'
-    setting:str = ':gear: Settings'
+    settings:str = ':gear: Settings'
     exit:str = ':cross_mark: Exit'
     
