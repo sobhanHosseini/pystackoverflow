4e38236cc06130cdaf0be284c2cceafcf4fdc775
SobhanHosseini <sobhan@Sobhan>
2023-02-26 15:57:36 +0330

    t

--- src/middleware/userMiddleware.py
+++ src/middleware/userMiddleware.py
@@ -6,7 +6,9 @@ class UserMiddleware(BaseMiddleware):
         self.update_types = ['message']
     
     def pre_process(self, message, data):
-        print('in pre process')
+        print(message)
+        print('-'*50)
+        print(data)
         
     def post_process(self, message, data, exception=None):
         print('in post process....')
