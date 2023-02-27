6743f72953e54ea31aebaba29784a73e72456b2f
SobhanHosseini <sobhan@Sobhan>
2023-02-27 10:18:41 +0330

    fix bug

--- src/dataClass/keyboards.py
+++ src/dataClass/keyboards.py
@@ -14,6 +14,7 @@ def create_keyboard(*keys, row_width=2, resize_keyboard=True):
         resize_keyboard=resize_keyboard
     )
     
+    keys = map(emoji.emojize, keys)
     buttons = map(types.KeyboardButton, keys)
     markup.add(*buttons)
     
