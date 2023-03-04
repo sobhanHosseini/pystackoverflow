c7bb56aef5761ba4d9cb81d9ee827aeb10fc538c
SobhanHosseini <hosseinisobhan@gmail.com>
2023-03-04 15:24:59 +0330

    fix bug

--- src/dataClass/inlineKeys.py
+++ src/dataClass/inlineKeys.py
@@ -2,9 +2,10 @@ from dataclasses import dataclass
 
 @dataclass
 class InlineKeys:
-    actions:str = 'Actions >>'
-    back:str = '<< Back'
+    actions:str = 'Actions :fast-forward_button:'
+    back:str = ':fast_reverse_button: Back'
     answer:str = ':bright_button: Answer'
     follow:str = ':plus: Follow'
     unfollow:str = ':minus: Unfollow'
     like:str = ':red_heart: Like'
+    
