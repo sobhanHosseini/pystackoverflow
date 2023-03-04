4177611373342499a7dfe1d22382618ed14a99eb
SobhanHosseini <hosseinisobhan@gmail.com>
2023-03-04 15:11:10 +0330

    test

--- src/dataClass/inlineKeys.py
+++ src/dataClass/inlineKeys.py
@@ -6,5 +6,5 @@ class InlineKeys:
     back:str = '<< Back'
     answer:str = ':bright_button: Answer'
     follow:str = ':plus: Follow'
-    unfollow:str = ':minus: Unfollow',
+    t:str = ':minus: Unfollow',
     like:str = ':red_heart: Like'
