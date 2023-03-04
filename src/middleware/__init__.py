8164546bfe7ccb253376574131330e94037606ba
SobhanHosseini <hosseinisobhan@gmail.com>
2023-03-04 13:51:57 +0330

    test

--- src/middleware/__init__.py
+++ src/middleware/__init__.py
@@ -2,7 +2,7 @@ from src.bot import bot
 from src.middleware.userMiddleware import UserMiddleware
 from src.middleware.callback_middleware import CallbackMiddleware
 
-# bot.setup_middleware([CallbackMiddleware(), UserMiddleware()])
+c = CallbackMiddleware()
+u = UserMiddleware()
 
-bot.middleware_handler.add(CallbackMiddleware())
-bot.middleware_handler.add(UserMiddleware())
+bot.setup_middleware([c, u])
