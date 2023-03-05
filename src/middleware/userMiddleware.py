d901bed6e54fccc38cc263dfd3cf8204c53959e3
SobhanHosseini <hosseinisobhan@gmail.com>
2023-03-05 11:41:47 +0330

    t

--- src/middleware/userMiddleware.py
+++ src/middleware/userMiddleware.py
@@ -14,16 +14,16 @@ class UserMiddleware(BaseMiddleware):
     
     def pre_process(self, message, data):
         chat_id = message.chat.id
-        msg = Message(
-            chat_id=chat_id,
-            bot=bot,
-            message_info=message
-            )
-        user = User(chat_id=chat_id)
+        # msg = Message(
+        #     chat_id=chat_id,
+        #     bot=bot,
+        #     message_info=message
+        #     )
+        # user = User(chat_id=chat_id)
         
-        data['user'] = user
-        data['message_sender'] = msg
-        data['chat_id'] = chat_id
+        # data['user'] = user
+        # data['message_sender'] = msg
+        # data['chat_id'] = chat_id
         # message.text = emoji.demojize(message.text)
         
     def post_process(self, message, data, exception=None):
